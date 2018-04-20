07da8ba284787697b8b4ed27cb254c4a12fad381
Sergio Leonardo Benitez Diaz <sergio.kun21@gmail.com>
2018-04-20 20:52:12 +0000

    JSES6: Code challenge description

---
+++ s07-es2015/town_challenge.js
@@ -0,0 +1,19 @@
+/**
+ * Suppose that you are working in a small town administration, and you are in charge of two elements:
+ * 1. Parks
+ * 2. Streets
+ * 
+ * It is a very small town, so rigth now there are only 3 parks and 4 streets. all parks and streets have a name and a build year.
+ * 
+ * At an end-of-year meeting, youur boss wants a final report with the following:
+ * 
+ * 1. Tree density of each park in the town (formula: number of tree/park area)
+ * 2. Average age of each town's park (formula: sum of all ages/number of parks)
+ * 3. The name of the park that has more than 1000 trees.
+ * 4. Total and average length of the town's streets.
+ * 5. Size classification of all streets: tiny/small/normal/big/huge. If the size is unknown, the default is normal.
+ * 
+ * All report data should be printed to the console
+ * 
+ * Hint: Use the next ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
+ */
