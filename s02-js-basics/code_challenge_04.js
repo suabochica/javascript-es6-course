361b1c45e5902e7ee81174a1de7bec5aa38bd5c7
Sergio L. Benítez D <sergio.kun21@gmail.com>
2018-12-21 17:42:46 -0500

    JSES6-S02
    code challenge four description

---
+++ s02-js-basics/code_challenge_04.js
@@ -0,0 +1,23 @@
+//-------------------------------------
+// Coding Challenge Four: Basketball average
+//-------------------------------------
+/**
+ * John and Mike both play basketball in different teams. In the latest 3 games, John's temas
+ * scored 89, 120 and 103 points, while Mike's team socored 116,94 and 123 points.
+ *
+ * 1. Calculate the average score for each team.
+ * 2. Decide which teams wins in average (highest average score), and print the winner to the console.
+ * 3. Then change the score to show different winners. Don't forget to take into accout there might be a draw
+ * 4. Extra: Mary also plays basketball, and her team socre 97, 134 and 105 points. Like before,
+ *    log the average winner to the console. Hint: You will need the && operator to take the decision.
+ */
+
+console.log('-------------------------------------------------')
+console.log('Coding Challenge Four - Start')
+console.log('-------------------------------------------------')
+
+// TODO
+
+console.log('-------------------------------------------------')
+console.log('Coding Challenge Four - End')
+console.log('-------------------------------------------------')
